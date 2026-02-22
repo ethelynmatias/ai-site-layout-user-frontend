79211563708dd4aab15492cb98512cc65375a81a
Ethelyn Matias <ethelyn.matias2015@gmail.com>
2026-02-22 14:24:38 +0800

    Cards

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import NewsLetter from './components/Newsletter';
+import Cards from './components/Cards';
 
 function App() {
   //const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <About />
       <NewsLetter />
+      <Cards />
     </>
   )
 }
---
+++ src/components/Cards.tsx
@@ -0,0 +1,11 @@
+const Cards = () =>{
+    return (
+        <div className="w-full py-[10rem] px-4 bg-white">
+            <div className="max-w[1240px] mx-auto ">
+
+            </div>
+        </div>
+    );
+}
+
+export default Cards
